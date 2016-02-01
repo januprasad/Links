bbe4a49b63ff3660fa6e7a7b516b1513418b1cdd
Richard Johnson <richard.johnson@mobme.in>
2016-02-01 11:28:15 +0530

    added edit and delete methods

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,13 +2,45 @@ class LinksController < ApplicationController
 	def new
 		@link = Link.new
 	end
-	def create
-		@link = Link.new
-		 
-	end
-
- def index
+    def index
     @links = Link.all
-  end
+    end
+    def create
+        @link = Link.new(link_params)
+         redirect_to(links_url)
+    end
 
+    def edit
+    @link = Link.find(params[:id])
+    end
+
+    def update
+    temp = Link.new(link_params)
+    @link = Link.find(params[:id])
+
+    @link.title = temp.title
+    @link.url = temp.url
+    @link.save    
+    temp.destroy
+
+    flash[:notice] = "File updated"
+    redirect_to links_url  
+     end
+
+    def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+    flash[:notice] = "File deleted"
+    redirect_to links_url    
+    end
+
+    def show
+    	# should redirect to the url paramtre of the respective link
+      	redirect_to links_url
+    end
+
+
+ def link_params
+    params.require(:link).permit(:title, :url)
+  end  
 end
