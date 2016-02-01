9d0ecec614ec73d47d49b605735eda7a6a83b114
Nakul-Joseph <nakuljsph@gmail.com>
2016-02-01 16:11:50 +0530

    already-sign-in-user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+
   def show
+  	if logged_in?
+  		redirect_to link_index_path
+  	end
+ end
+
+ 	  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
+
+    # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
